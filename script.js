a0ac6fc2f779a5e123a478331e237f927c7f15ff
khardik07 <89795974+khardik07@users.noreply.github.com>
2024-10-26 13:03:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,56 @@
 let cityInput = document.getElementById('city_input'),
-searchBtn = document.getElementById('searchBtn'),
-api_key = '23283e0813d6d0a757ecf65aa678f26b',
-currentWeatherCard=document.querySelectorAll('.weather-left .card')[0]; 
-function  getWeatherDetails(name, lat, lon, country, state){
- let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=23283e0813d6d0a757ecf65aa678f26b`,
- WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=23283e0813d6d0a757ecf65aa678f26b`,
- days = [
-   'Sunday',
-   'Monday',
-   'Tuesday',
-   'wednesday',
-   'Thursday',
-   'Friday',
-   'Saturday'
- ],
- months = [
-   'Jan',
-   'Feb',
-   'Mar',
-   'Apr',
-   'May',
-   'Jun',
-   'Jul',
-   'Aug',
-   'Sep',
-   'Oct',
-   'Nov',
-   'Dec'
- ];
- fetch(WEATHER_API_URL).then(res => res.json()).then(data =>{
-let date =new Date();
-currentWeatherCard.innerHTML=`
-<div class="current-weather">
-                        <div class="details">
-                            <p>Now</p>
-                            <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;</h2>
-                            <p>${data.weather[0].description}</p>
-                        </div>
-                        <div class="weather-icon">
-                            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
-                        </div>
+searchBtn = document.getElementById('searchBtn');
+const apiKey = "23283e0813d6d0a757ecf65aa678f26b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+let currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
+
+function getWeatherDetails(name, lat, lon, country) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let date = new Date();
+
+    fetch(`${apiUrl}${name}&appid=${apiKey}`)
+        .then(res => res.json())
+        .then(data => {
+            currentWeatherCard.innerHTML = `
+                <div class="current-weather">
+                    <div class="details">
+                        <p>Now</p>
+                        <h2>${data.main.temp.toFixed(2)}&deg;C</h2>
+                        <p>${data.weather[0].description}</p>
                     </div>
-                    <hr>
-                    <div class="card-footer">
-                        <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()}, ${date.getMonth()}, ${date.getFullYear()}</p>
-                        <p><i class="fa-light fa-location-dot"></i>${name}, ${country}</p>
+                    <div class="weather-icon">
+                        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
                     </div>
-`;
- }).catch(() => {
-   alert('Failed to fetch current weather');
- });
+                </div>
+                <hr>
+                <div class="card-footer">
+                    <p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()}</p>
+                    <p><i class="fa-light fa-location-dot"></i>${name}, ${country}</p>
+                </div>
+            `;
+        })
+        .catch(() => {
+            alert('Failed to fetch current weather');
+        });
 }
-function getCityCoordinates(){
-    let cityName=cityInput.value.trim();
-   cityInput.value='';
-   if(!cityName) return;
-   let GEOCODING_API_URL_ = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=23283e0813d6d0a757ecf65aa678f26b`;
-   fetch(GEOCODING_API_URL_).then(res => res.json()).then(data =>{
-     let {name, lat, lon, country, state} = data[0];
-     getWeatherDetails(name, lat, lon, country, state);
-   }).catch(() =>{
-    alert(`Failed to fetch coordinates of ${cityName}`)
-   }); 
+
+function getCityCoordinates() {
+    let cityName = cityInput.value.trim();
+    cityInput.value = '';
+    if (!cityName) return;
+
+    const GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+    fetch(GEOCODING_API_URL)
+        .then(res => res.json())
+        .then(data => {
+            let { name, coord: { lat, lon }, sys: { country } } = data;
+            getWeatherDetails(name, lat, lon, country);
+        })
+        .catch(() => {
+            alert(`Failed to fetch coordinates of ${cityName}`);
+        });
 }
 
 searchBtn.addEventListener('click', getCityCoordinates);
-
